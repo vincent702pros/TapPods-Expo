3db51caf122aea5501f58671677747f1269ec606
Vincent <vincentbaylon@gmail.com>
2022-12-14 22:09:09 -0800

    Readded genre card logic

--- src/components/GenreCard.js
+++ src/components/GenreCard.js
@@ -1,6 +1,6 @@
-import { Text, View, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import { GenreListContext } from "../GenreListContext";
 
 export function GenreCard({ title }) {
@@ -11,89 +11,88 @@ export function GenreCard({ title }) {
   );
 }
 
-// export const GCard = (props) => {
+export const GCard = (props) => {
+  const [isChecked, setCheck] = useState(false);
+  const { genreList, setGenreList } = useContext(GenreListContext);
 
-//   const [isChecked, setCheck] = useState(false);
-//   const {genreList, setGenreList} = useContext(GenreListContext);
+  const toggleCheck = () => {
+    if (isChecked == true) {
+      //Remove Checked Genre
+      setGenreList(genreList.filter((genre) => genre !== props.title));
+      setCheck(false);
+      return;
+    }
 
-//   const toggleCheck = () => {
+    //Add Checked Genere
+    setGenreList([...genreList, props.title]);
+    setCheck(true);
+  };
 
-//     if(isChecked == true) {
-//       //Remove Checked Genre
-//       setGenreList(genreList.filter(genre => genre !== props.title));
-//       setCheck(false);
-//       return;
-//     }
+  return (
+    <TouchableWithoutFeedback onPress={toggleCheck}>
+      <View style={styles.card}>
+        <Ionicons
+          style={{
+            opacity: isChecked ? 1 : 0,
+            position: "absolute",
+            top: -6,
+            right: -6,
+            backgroundColor: "#fff",
+            borderRadius: 500,
+            padding: 2,
+          }}
+          name="checkmark-sharp"
+          size={25}
+          color="#000"
+        />
 
-//     //Add Checked Genere
-//     setGenreList([...genreList, props.title]);
-//     setCheck(true);
+        <Text style={styles.cardTitle}>{props.title}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
-//   }
+export const RenderGenres = (props) => {
+  //API Request
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
-//   return (
-//     <TouchableWithoutFeedback onPress={toggleCheck}>
-//         <View style={styles.card}>
+  const getGenere = async () => {
+    try {
+      const response = await fetch(
+        "https://www.tappods.com/wp-json/tpapi/v1/podgenere/?key=tpKEY123"
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-//             <Ionicons
-//               style={{
-//                 opacity: isChecked ? 1 : 0,
-//                 position: "absolute",
-//                 top: -6,
-//                 right: -6,
-//                 backgroundColor: '#fff',
-//                 borderRadius: 500,
-//                 padding: 2,
-//               }}
-//               name="checkmark-sharp"
-//               size={25}
-//               color="#000"/>
+  useEffect(() => {
+    getGenere();
+  }, []);
 
-//             <Text style={styles.cardTitle}>{props.title}</Text>
-//         </View>
-//     </TouchableWithoutFeedback>
-//   );
-// }
-
-// export const RenderGenres = (props) => {
-
-//   //API Request
-//   const [isLoading, setLoading] = useState(true);
-//   const [data, setData] = useState([]);
-
-//   const getGenere = async () => {
-//      try {
-//       const response = await fetch('https://www.tappods.com/wp-json/tpapi/v1/podgenere/?key=tpKEY123');
-//       const json = await response.json();
-//       setData(json);
-//     } catch (error) {
-//       console.error(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
-
-//   useEffect(() => {
-//     getGenere();
-//   }, []);
-
-//   //Display Results
-//   return(
-//       <View style={{
-//         flexDirection: 'row',
-//         flex: 1,
-//         flexWrap: "wrap",
-//         paddingTop: 10
-//         }}>
-//         {isLoading ? ( <Text>Loading</Text> ) :(
-//           data.map((item)=>(
-//             <GCard title={item}/>
-//           ))
-//         )}
-//       </View>
-//   );
-
-// }
+  //Display Results
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        flex: 1,
+        flexWrap: "wrap",
+        paddingTop: 10,
+      }}
+    >
+      {isLoading ? (
+        <Text>Loading</Text>
+      ) : (
+        data.map((item) => <GCard title={item} />)
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
